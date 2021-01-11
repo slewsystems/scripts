56eba0fe28d7cff0257ce8598143ce358ddd1e58
Brandon Patram <brandon@slewsystems.com>
2021-01-11 10:31:31 -0500

    focus terminal window when opening

--- git/extern-branch-tidy.sh
+++ git/extern-branch-tidy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")/branch-tidy.sh
-osascript -e "tell application \"Terminal\" to do script \"$SCRIPT_PATH $*; sleep 3; exit;\""
+osascript -e "tell application \"Terminal\"
+    activate
+    do script \"$SCRIPT_PATH $*; sleep 3 && exit;\"
+end tell"
