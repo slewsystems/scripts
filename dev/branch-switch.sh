cdac2a7af1a43c8d7e5204a9ee8d4ffad73cbe99
Brandon Patram <brandonpatram@gmail.com>
2024-05-20 14:38:22 -0400

    update default db service name

--- dev/branch-switch.sh
+++ dev/branch-switch.sh
@@ -14,7 +14,7 @@
 # ---------------------------
 
 APP_DIRECTORY=${1:-$PWD}
-DATABASE_DOCKER_SERVICE_NAME="postgres13"
+DATABASE_DOCKER_SERVICE_NAME="postgres14"
 BUNDLER_GEMFILE_LOCK_FILE="Gemfile.lock"
 RUBY_VERSION_FILE=".ruby-version"
 NODE_VERSION_FILE=".node-version"
